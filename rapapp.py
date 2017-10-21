d009efc664a7ce18a0ff48733a90337b481e2880
Michelle Caplin <mcaplin@ucsd.edu>
2017-10-21 16:12:46 -0700

    boii

--- rapapp.py
+++ rapapp.py
@@ -1,16 +1,51 @@
 import pronouncing as pro
 import os
 import random
+import re
+import wikipedia
+import sys
 
-text = "My name is poop I eat soup Fidget spinner throw your mom through a hoop mom tom"
-text = "Python is a widely used high-level programming language for general-purpose programming, created by Guido van Rossum and first released in 1991. An interpreted language, Python has a design philosophy that emphasizes code readability notably using whitespace indentation to delimit code blocks rather than curly brackets or keywords, and a syntax that allows programmers to express concepts in fewer lines of code than might be used in languages such as C++ or Java. The language provides constructs intended to enable writing clear programs on both a small and large scale."
+if len(sys.argv) < 2:
+    print "Choose a topic, homie"
+    exit(0)
+text = ""   
+    
+    
+def GetArticle():
+    article = sys.argv[1]
 
-rhymes = {}
+    results = wikipedia.search(article)
 
-def findRhymes():
+    i = 1
+    p = ""
+
+    try:
+        p = wikipedia.summary(sys.argv[1], sentences=10)
+    except:
+        while(i < len(results)):
+            try: 
+                p = wikipedia.summary(results[i], sentences=10)
+            except: 
+                i+=1
+            finally:
+                print "\n" + results[i] + ": A Rap auto-generated from Wikipedia by Rap-App\n"
+                p = wikipedia.summary(results[i], sentences=10)
+                break
+    finally:
+        print "\n" + sys.argv[1] + ": A Rap auto-generated from Wikipedia by Rap-App\n"
+        p = wikipedia.summary(sys.argv[1], sentences=10)
+    return p
+        
+    
+
+
+
+def FindRhymes(text):
+    rhymes = {}
     words = text.split()
     for i in range(len(words)):#word in words:
         word = words[i]
+        
         r = pro.rhymes(word)[:1]
         if len(r) > 0:
             phrase = ""
@@ -27,39 +62,59 @@ def findRhymes():
             else:
                 rhymes[r[0]].append(phrase)
                 #print rhymes[r[0]]
+    return rhymes
 
-"""def getPhrases():
+
+"""
+text = "I am ok with your excellent egg puns, I don't exaggerate my utter joy that I cannot explain. This is not a joke"          
+
+
+def getEggPuns():
+    words = text.split()
+    for i in range(len(words)):
+        word = words[i]
+        word = re.sub(r'(\.*)ex(\.*)', '\1eggs\2', word)
+        word = re.sub(r'(\.*)j*ok(\.*)', '\1yolk\2', word)
+        words[i] = word
+    return " ".join(words)
+print text + "\n\n\n"
+print getEggPuns()
+    """
+    
+    
+    
+
+
+def MakeRap(rhymes):
+    rap = ""
     for key in rhymes:
         if len(rhymes[key]) > 1:
-                """
-            
-    
-    
-    
-    
-    
-findRhymes()
-#print rhymes
+            x = [[i] for i in range(len(rhymes[key]))]
+            random.shuffle(x)
+            inarow = 0
+            last_rhyme = ""
+            for j in range(len(rhymes[key])):
+                rhyme = rhymes[key][j]
+                r_list = rhyme.split()
+                r = r_list[len(r_list)-1]
+                if last_rhyme == r:
+                    inarow += 1
+                else:
+                    inarow = 0
+                last_rhyme = r
+                if inarow < 2:
+                    rap += rhyme + ". \n"
+    return rap
+                  
+                  
+def main():  
+    text = GetArticle()
+    rhymes = FindRhymes(text)
+    rap = MakeRap(rhymes)
 
-        #rhymes[r]=word
-#os.system("say 'test'")
-done = ""
-out = ""
-for key in rhymes:
-    if len(rhymes[key]) > 1:
-        for rhyme in rhymes[key]:
-            out += rhyme + ". "
-    #t+=". "
-    #os.system("say 'key'" )
-    #done += " ".join(rhymes[key])
- #   done+= ". "
-    #print key + " " + str(rhymes[key])
-    #print str(rhymes[key])
-#print rhymes
-print out
-os.system("say \"%s\" -v Zarvox" % out)
+    print rap
 
-#print rhymes
-"""
-find all the rhymes, make array of each rhyme string with the 1-8 words before it
-"""
+    os.system("say '%s' " % rap)
+
+
+main()
